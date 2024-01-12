2bc66343958dfb4e7ee49de10a5cfd797b70c518
Allex Antoniollo <antoniolloallex@gmail.com>
2024-01-12 11:59:17 -0300

    Login

--- artchain/src/components/header.tsx
+++ artchain/src/components/header.tsx
@@ -3,8 +3,28 @@ import Image from "next/image";
 import Link from "next/link";
 import { CiSearch } from "react-icons/ci";
 import { IoIosArrowDown } from "react-icons/io";
+import { doLogin } from "@/services/Web3Service";
 import DarkModeButton from "./darkModeButton";
+import { RxExit } from "react-icons/rx";
+
+import { useState } from "react";
 export default function Header() {
+  const [wallet, setWallet] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  async function btnLoginClick() {
+    try {
+      const result = await doLogin();
+
+      if (result !== null && result !== undefined) setWallet(result);
+      else setError("Wallet not found/allowed.");
+    } catch (err) {
+      setError((err as Error).message);
+    }
+  }
+  async function btnLogout() {
+    alert("Logout");
+  }
+
   return (
     <header className="shadow-sl border-b border-gray-200 flex items-center justify-evenly w-full py-4 px-2 ">
       <Link href="/">
@@ -38,9 +58,27 @@ export default function Header() {
         SELL
       </Link>
       <DarkModeButton></DarkModeButton>
-      <button className="ease-linear duration-150 flex items-center border border-purple-900 justify-center rounded bg-white px-3 py-2 hover:bg-purple-900 hover:text-white hover:shadow-md ">
-        <p>Connect Wallet</p>
-      </button>
+      {!wallet ? (
+        <button
+          onClick={btnLoginClick}
+          className="ease-linear duration-150 flex items-center border border-purple-900 justify-center rounded bg-white px-3 py-2 hover:bg-purple-900 hover:text-white hover:shadow-md "
+        >
+          <p>Connect Wallet</p>
+        </button>
+      ) : (
+        <>
+          <Link
+            className="bg-purple-900 text-white rounded p-3"
+            href="/profile"
+          >
+            You're Connected
+          </Link>
+          <RxExit
+            onClick={btnLogout}
+            className="cursor-pointer size-8"
+          ></RxExit>
+        </>
+      )}
     </header>
   );
 }
--- artchain/src/services/Web3Service.ts
+++ artchain/src/services/Web3Service.ts
@@ -1,5 +1,52 @@
 import axios from "axios";
 import { ethers } from "ethers";
+import MarketABI from "./Market.abi.json";
+import Erc721ABI from "./Erc721.abi.json";
+import Erc1155ABI from "./Erc1155.abi.json";
+
+const MARKETPLACE_ADDRESS = `${process.env.MARKETPLACE_ADDRESS}`;
+const ERC721 = `${process.env.ERC721}`;
+const ERC1155 = `${process.env.ERC1155}`;
+
+function getProvider(): ethers.BrowserProvider {
+  if (!window.ethereum) throw new Error("No MetaMask found");
+  return new ethers.BrowserProvider(window.ethereum);
+}
+
+function getContract(provider?: ethers.BrowserProvider): ethers.Contract {
+  if (!provider) provider = getProvider();
+  return new ethers.Contract(
+    MARKETPLACE_ADDRESS,
+    MarketABI as ethers.InterfaceAbi,
+    provider
+  );
+}
+
+async function getContractSigner(
+  provider?: ethers.BrowserProvider
+): Promise<ethers.Contract> {
+  if (!provider) provider = getProvider();
+  const signer = await provider.getSigner(
+    localStorage.getItem("wallet") || undefined
+  );
+  const contract = new ethers.Contract(
+    MARKETPLACE_ADDRESS,
+    MarketABI as ethers.InterfaceAbi,
+    provider
+  );
+  return contract.connect(signer) as ethers.Contract;
+}
+
+export async function doLogin() {
+  const provider = getProvider();
+  const account = await provider.send("eth_requestAccounts", []);
+
+  if (!account || !account.length) throw new Error("Wallet not found/allowed.");
+
+  localStorage.setItem("wallet", account[0]);
+
+  return account[0];
+}
 
 export type NewNFT721 = {
   name?: string;
@@ -57,5 +104,15 @@ export async function uploadAndCreate(nft: NewNFT721): Promise<number> {
 
   //Mint
 
+  return 1;
+}
+
+type NftSale = {
+  price?: number;
+  tokenId: number;
+  nftAddress: string;
+};
+
+export async function sellNFT(nft: NftSale): Promise<number> {
   return 1;
 }
